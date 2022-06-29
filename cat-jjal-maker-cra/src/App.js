1bdae79c17a643125931dfffbc1a0d7dc341c65b
soveloper <ksh1210sh@naver.com>
2022-06-29 22:33:58 +0900

    커밋 테스트

--- cat-jjal-maker-cra/src/App.js
+++ cat-jjal-maker-cra/src/App.js
@@ -29,7 +29,7 @@ function CatItem(props) {
 
 function Favorites({ favorites }) {
   if (favorites.length === 0) {
-    return <div>사진 위 하트를 눌러 고양이 사진을 저장해봐요!</div>
+    return <div>사진 위 하트를 눌러 고양이 사진을 저장해봐요!!!!!!</div>
   }
 
   return (
